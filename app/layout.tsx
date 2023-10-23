88af4dda7aadb2b919fb4d872550e048672a6902
Ryan Green <ryang@ryang.ca>
2023-10-23 12:45:11 -0230

    Title change

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import { Inter } from 'next/font/google'
 
 export const metadata = {
   metadataBase: new URL('https://postgres-prisma.vercel.app'),
-  title: 'Vercel Postgres Demo with Prisma',
+  title: 'Tracking Metrobus',
   description:
-    'A simple Next.js app with Vercel Postgres as the database and Prisma as the ORM',
+    'Tracking Metrobus in St. John\'s, NL,
 }
 
 const inter = Inter({
